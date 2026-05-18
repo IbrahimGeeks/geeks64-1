085977c9426e16f621be9fb73df7213c9baf8e92
Ibrahim_geeks <fourdeltaone100@gmail.com>
2026-05-18 18:33:45 +0600

    .

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('message/', views.FirstMessageView.as_view(), name='message'),
+    path('emoji/', views.emoji, name='emoji'),
+    path('', views.BlogListView.as_view(), name='blog_lst'),
+    path('blog_list/<int:id>/', views.BlogDetailView.as_view(), name='blog_id'),
+    path('search/', views.SearchView.as_view(), name='search'),
+]
